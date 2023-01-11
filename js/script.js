59a5c58b275907e64d838af5201247c216ef34cf
Alex <55253968+Alex275054@users.noreply.github.com>
2023-01-11 11:32:51 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -129,6 +129,243 @@ $(document).ready(function () {
 // slick
 //==========================================================================================================================================
 // Инициализируем Swiper
+new Swiper('.about-slider', {
+	// Стрелки
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	// Навигация 
+	// Буллеты, текущее положение, прогрессбар
+	pagination: {
+		el: '.swiper-pagination',
+		// Буллеты
+		type: 'bullets',
+		clickable: true,
+		// Динамические буллеты
+		dynamicBullets: true,
+		// // Кастомные буллеты
+		// renderBullet: function (index, className) {
+		// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+		// },
+		/*
+		// Фракция
+		type: 'fraction',
+		// Кастомный вывод фракции
+		renderFraction: function (currentClass, totalClass) {
+			return 'Фото <span class="' + currentClass + '"></span>' +
+				' из ' +
+				'<span class="' + totalClass + '"></span>';
+		},
+		*/
+		// Прогрессбар
+		//type: 'progressbar'
+	},
+	// Бесконечный слайдер
+	loop: true,
+	// Переключение при клике на слайд
+	slideToClickedSlide: true,
+	// Управление клавиатурой
+	keyboard: {
+		// Включить\выключить
+		enabled: true,
+		// Включить\выключить
+		// только когда слайдер
+		// в пределах вьюпорта
+		onlyInViewport: true,
+		// Включить\выключить
+		// управление клавишами
+		// pageUp, pageDown
+		pageUpDown: true,
+	},
+
+	// Управление колесом мыши
+	// mousewheel: {
+	// 	// Чувствительность колеса мыши
+	// 	sensitivity: 1,
+	// 	// Класс объекта на котором
+	// 	// будет срабатывать прокрутка мышью.
+	// 	//eventsTarget: ".image-slider"
+	// },
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 1,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 1,
+
+	// Скорость
+	speed: 500,
+
+	// Эффекты переключения слайдов.
+	// Листание
+	// effect: 'slide',
+
+	/*
+	// Эффекты переключения слайдов.
+	// Cмена прозрачности
+	effect: 'fade',
+
+	// Дополнение к fade
+	fadeEffect: {
+		// Параллельная
+		// смена прозрачности
+		crossFade: true
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Переворот
+	effect: 'flip',
+
+	// Дополнение к flip
+	flipEffect: {
+		// Тень
+		slideShadows: true,
+		// Показ только активного слайда
+		limitRotation: true
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Куб
+	effect: 'cube',
+
+	// Дополнение к cube
+	cubeEffect: {
+		// Настройки тени
+		slideShadows: true,
+		shadow: true,
+		shadowOffset: 20,
+		shadowScale: 0.94
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Эффект потока
+	effect: 'coverflow',
+
+	// Дополнение к coverflow
+	coverflowEffect: {
+		// Угол
+		rotate: 20,
+		// Наложение
+		stretch: 50,
+		// Тень
+		slideShadows: true,
+	},
+	*/
+	/*
+	// Брейк поинты (адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		480: {
+			slidesPerView: 2,
+		},
+		992: {
+			slidesPerView: 3,
+		}
+	},
+	*/
+	/*
+	// Брейк поинты (адаптив)
+	// Соотношение сторон
+	breakpoints: {
+		'@0.75': {
+			slidesPerView: 1,
+		},
+		'@1.00': {
+			slidesPerView: 2,
+		},
+		'@1.50': {
+			slidesPerView: 3,
+		}
+	},
+	*/
+
+	// Миниатюры (превью)
+	/*
+	thumbs: {
+		// Свайпер с мениатюрами
+		// и его настройки
+		swiper: {
+			el: '.image-mini-slider',
+			slidesPerView: 5,
+		}
+	},
+	*/
+	/*
+	// Передача управления
+	controller: {
+		control: myTextSlider
+	},
+	*/
+
+	// Обновить свайпер
+	// при изменении элементов слайдера
+	observer: true,
+
+	// Обновить свайпер
+	// при изменении родительских
+	// элементов слайдера
+	observeParents: true,
+
+	// Обновить свайпер
+	// при изменении дочерних
+	// элементов слайда
+	observeSlideChildren: true,
+
+	// Виртуальные слайды
+	// формирование слайдера
+	/*
+	virtual: {
+		slides: (function () {
+			let slide = []
+			for (let i = 0; i < 500; i++) {
+				slide.push(`<div class="image-slider__text">Слайд №${i}</div>`);
+			}
+			return slide;
+		}()),
+	},
+	*/
+	/*
+	// Доступность
+	a11y: {
+		// Включить/выключить
+		enabled: true,
+		// Сообщения
+		prevSlideMessage: 'Previous slide',
+		nextSlideMessage: 'Next slide',
+		firstSlideMessage: 'This is the first slide',
+		lastSlideMessage: 'This is the last slide',
+		paginationBulletMessage: 'Go to slide {{index}}',
+		notificationClass: 'swiper-notification',
+		containerMessage: '',
+		containerRoleDescriptionMessage: '',
+		itemRoleDescriptionMessage: '',
+		// и т.д.
+	},
+	*/
+	/*
+	// События
+	on: {
+		// Событие инициализации
+		init: function () {
+			console.log('Слайдер запущен!');
+		},
+		// Событие смены слайда
+		slideChange: function () {
+			console.log('Слайд переключен');
+		}
+	},
+	*/
+});
 let myImageSlider = new Swiper('.wps__slider', {
 	// Стрелки
 	// navigation: {
